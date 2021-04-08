370320b8377358d6e6f7cfa2f538fd528d9666ef
wenbo <iwoffee@gmail.com>
2021-04-08 17:04:00 -0400

    add auc

--- evaluation.py
+++ evaluation.py
@@ -37,8 +37,102 @@ recip_rank               all     0.1999
 import argparse
 import os
 import sys
-
+import numpy as np
+import math
 import pytrec_eval
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score
+
+
+def min_max(arr):
+    mi = np.min(arr)
+    ma = np.max(arr)
+    res = []
+    for x in arr:
+        res.append( float(x - mi) / (ma - mi) )
+    return res
+
+
+def calc_auc(qrel_file, pred_file):
+    # read ground truth file
+    q_answers = {}
+    with open(qrel_file, "r") as f:
+        lines = f.readlines()
+
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                continue
+
+            qid, Q0, did, relevant = line.split(" ")
+
+            if relevant == "1":
+                if qid in q_answers.keys():
+                    q_answers[qid].append(did)
+                else:
+                    q_answers[qid] = [did]
+
+    # read pred file
+    q_pred = {}
+    with open(pred_file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                continue
+
+            arr = line.split("\t")
+            qid = arr[0]
+            did = arr[2]
+            rank = int(arr[3])
+            score = float(arr[4])
+            if qid in q_pred.keys():
+                q_pred[qid].append([did, score])
+            else:
+                q_pred[qid] = [[did, score]]
+            # print("did:",did,", score:", score)
+
+    # auc
+    auc_list = []
+
+    for qid in q_pred.keys():
+        y_true = []
+        y_score = []
+        ans = q_answers[qid]
+        for did, score in q_pred[qid]:
+            if did in ans:
+                y_true.append(1)
+            else:
+                y_true.append(0)
+
+            y_score.append(score)
+
+        y_score = min_max(y_score)
+        y_score = np.array(y_score)
+        y_true = np.array(y_true)
+
+        # 下面两种方式计算auc结果相同。但方式二能返回更多信息。
+
+        # 方式 1
+        # auc = roc_auc_score(y_true, y_score)
+
+        # 方式 2
+        fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
+        auc = metrics.auc(fpr, tpr)
+        if math.isnan(auc):
+            auc = 0
+
+        auc_list.append(auc)
+        # print("qid: %s  -  auc:%.4f" % (qid, auc))
+        # if qid == "1":
+        #     pass
+
+    final_auc = np.mean(auc_list)
+    print("=== AUC Evaluation results ===")
+    print("Test total:", len(auc_list))
+    print("Prediction file:", pred_file)
+    # print("len(pred.keys):", len(q_pred.keys()))
+    print("auc =", final_auc)
 
 
 def main():
@@ -93,6 +187,8 @@ def main():
                     measure,
                     [query_measures[measure]
                      for query_measures in results.values()]))
+
+    calc_auc(args.qrel, args.run)
 
 if __name__ == "__main__":
     sys.exit(main())
