be3a9d61718be6671b084f497877a2b1038a9b25
wenbo <iwoffee@gmail.com>
2021-04-08 01:53:34 -0400

    add args

--- learning2rank.py
+++ learning2rank.py
@@ -17,6 +17,9 @@ if __name__ == '__main__':
     parser.add_argument('--model_type', help='model_type', type=str, default='lambdaMART')
     parser.add_argument('--ranklib_path', help="ranklib_path", type=str, default='')
 
+    parser.add_argument('--train_file', help="train_file", type=str, default='')
+    parser.add_argument('--test_file', help="test_file", type=str, default='')
+
 
     parser.add_argument('--train', action='store_true', help="train learning2rank model")
     parser.add_argument('--pred', action='store_true', help="do predict")
@@ -41,8 +44,8 @@ if __name__ == '__main__':
     train_metric = 'MAP'
     train_model = args.model_type
 
-    data_train_path = "for_ltr/ltr_%s_train.txt" % data_type
-    data_test_path = "for_ltr/ltr_%s_test.txt" % data_type
+    data_train_path = args.train_file
+    data_test_path = args.test_file
 
     save_model_path = BASE_DIR + '/ltr/models/' + data_type + "_" + train_model + '_' + train_metric + ".txt"
     data_pred_path = BASE_DIR + '/ltr/predicts/' + data_type + "_" + train_model + '_' + train_metric + "_pred.txt"
