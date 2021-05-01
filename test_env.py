33b7a13d71baafbd724307127a16e759b92e2b80
wenbo <iwoffee@gmail.com>
2021-05-01 18:46:00 -0400

    add test file

---
+++ test_env.py
@@ -0,0 +1,13 @@
+import h5py
+import nltk
+import tensorflow
+import keras
+import gensim
+import sklearn
+
+print("h5py:", h5py.__version__)
+print("nltk:", nltk.__version__)
+print("tensorflow:", tensorflow.__version__)
+print("keras:", keras.__version__)
+print("gensim:", gensim.__version__)
+print("sklearn:", sklearn.__version__)
